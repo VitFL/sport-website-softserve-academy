10cfa1fd04fb454333ee7f1f48a57108eb3d9c9a
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-06-08 18:41:41 +0300

    localStore functionality

--- pages/fans/js/index.js
+++ pages/fans/js/index.js
@@ -5,80 +5,136 @@ const formUserName = document.getElementById("userName");
 const formUserComment = document.getElementById("userComment");
 
 window.onload = () => {
-  fetchLocalStorage().forEach(comment => { 
-    const {timestamp, userName, userComment} = JSON.parse(comment);
-    saveToDB(timestamp, userName, userComment) ? deleteFromLocalStorage(`comment-${timestamp}`) : null;
-  });  
+  fetchLocalStorage().forEach(comment => {
+    const { timestamp, userName, userComment } = JSON.parse(comment);
+    saveToDB(timestamp, userName, userComment)
+      ? deleteFromLocalStorage(`comment-${timestamp}`)
+      : null;
+  });
   fetchDB();
-}
+};
 
 const fetchDB = () => {
   const commentsStub = [
-    {timestamp: 1558477306623, userName: 'Ivan Kokos', userComment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, voluptate pariatur? Consectetur veniam ex vitae asperiores odit libero hic dicta consequuntur quam possimus. Animi aliquam, error perspiciatis expedita numquam voluptate?'},
-    {timestamp: 1558349896234, userName: 'Ihor Dodon', userComment: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ea architecto dolorem maxime eveniet aut voluptatibus, minus corporis aliquam unde ut! Nemo cumque laboriosam earum non culpa ullam sint sapiente.'},
-    {timestamp: 1557745096234, userName: 'Vasia Dubilet', userComment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore et fugit itaque dicta natus doloremque rerum reprehenderit ipsam corporis ipsum eos illo asperiores atque, maiores mollitia blanditiis nostrum soluta minus!'}
-  ]
-  commentsStub.forEach(comment => {
-    const commentHTML = getCommentHTML(comment.timestamp, comment.userName, comment.userComment);
-    newCommentToDOM(commentHTML);
+    {
+      timestamp: 1558477306623,
+      userName: "Ivan Kokos",
+      userComment:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, voluptate pariatur? Consectetur veniam ex vitae asperiores odit libero hic dicta consequuntur quam possimus. Animi aliquam, error perspiciatis expedita numquam voluptate?"
+    },
+    {
+      timestamp: 1558349896234,
+      userName: "Ihor Dodon",
+      userComment:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ea architecto dolorem maxime eveniet aut voluptatibus, minus corporis aliquam unde ut! Nemo cumque laboriosam earum non culpa ullam sint sapiente."
+    },
+    {
+      timestamp: 1557745096234,
+      userName: "Vasia Dubilet",
+      userComment:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore et fugit itaque dicta natus doloremque rerum reprehenderit ipsam corporis ipsum eos illo asperiores atque, maiores mollitia blanditiis nostrum soluta minus!"
     }
-  )
-}
+  ];
+  commentsStub.forEach(comment => {
+    const commentHTML = getCommentHTML(
+      comment.timestamp,
+      comment.userName,
+      comment.userComment
+    );
+    newCommentToDOM(commentHTML);
+  });
+};
 
 const saveToDB = (timestamp, userName, userComment) => {
-  const value = JSON.stringify({timestamp, userName, userComment});
-  const key = `comment-${timestamp}`;  
-  console.log('Saved to DB:')
-  console.log(value);
+  const value = JSON.stringify({ timestamp, userName, userComment });
+  const key = `comment-${timestamp}`;
+  createAlert(
+    "",
+    "Success!",
+    "User review successfully saved to DB.",
+    "success",
+    true,
+    true,
+    "pageMessages"
+  );
   return true;
-}
+};
 
 const fetchLocalStorage = () => {
   let comments = Object.keys(localStorage)
-  .filter(key => key.startsWith('comment-'))
-  .map(key => localStorage[key]);
+    .filter(key => key.startsWith("comment-"))
+    .map(key => localStorage[key]);
   return comments;
-}
-
-const saveToLocalStorage = (timestamp, userName, userComment) => {
-  const value = JSON.stringify({timestamp, userName, userComment});
-  const key = `comment-${timestamp}`;  
-  localStorage.setItem(key, value);
-}
-
-const deleteFromLocalStorage = (key) => {
-  console.log('deleted');
-  localStorage.removeItem(key);
-  removeFromDOM(key);
 };
 
-const newCommentToDOM = (commentHTML) => {
-  commentsContainer.insertAdjacentHTML('beforeend', commentHTML); 
-}
+const saveToLocalStorage = (timestamp, userName, userComment) => {
+  const value = JSON.stringify({ timestamp, userName, userComment });
+  const key = `comment-${timestamp}`;
+  localStorage.setItem(key, value);
+  createAlert(
+    "",
+    "Ohh, snap!",
+    "Since we lost internet connection, user review was saved to local state.",
+    "warning",
+    true,
+    true,
+    "pageMessages"
+  );
+  return localStorage.hasOwnProperty(key);
+};
 
-const removeFromDOM = (elementId) => {
+const deleteFromLocalStorage = key => {
+  console.log("deleted");
+  localStorage.removeItem(key);
+  removeFromDOM(key);
+  createAlert(
+    "",
+    "Info",
+    "User review was removed from localStorage.",
+    "info",
+    true,
+    true,
+    "pageMessages"
+  );
+  return !localStorage.hasOwnProperty(key);
+};
+
+const newCommentToDOM = commentHTML => {
+  commentsContainer.insertAdjacentHTML("beforeend", commentHTML);
+};
+
+const removeFromDOM = elementId => {
   const element = document.getElementById(elementId);
   if (element) element.parentNode.removeChild(element);
-}
+};
 
-
-const getCommentHTML = (timestamp, userName, userComment, isPending = false) => {
+const getCommentHTML = (
+  timestamp,
+  userName,
+  userComment,
+  isPending = false
+) => {
   const date = getDateFromTimestamp(timestamp);
-  const newComment = `<div class="mb-4 p-3 d-flex justify-content-between ${isPending ? "bg-warning text-white" : ""}"${isPending ? `id="comment-${timestamp}"` : ""}>
+  const newComment = `<div class="mb-4 p-3 d-flex justify-content-between ${
+    isPending ? "bg-warning text-white" : ""
+  }"${isPending ? `id="comment-${timestamp}"` : ""}>
   <div class="d-flex flex-column w-100"><p>
   ${userComment}
   </p>
   <div class="d-flex justify-content-between"><span>${date}</span><strong>
   ${userName}
   </strong></div></div>
-  ${isPending ? `<div class="ml-4 d-flex align-items-center"><button type="button" class="close" aria-label="Close" onclick="deleteFromLocalStorage('comment-${timestamp}')">
+  ${
+    isPending
+      ? `<div class="ml-4 d-flex align-items-center"><button type="button" class="close" aria-label="Close" onclick="deleteFromLocalStorage('comment-${timestamp}')">
   <span aria-hidden="true">&times;</span>
-</button></div>` : ""}
+</button></div>`
+      : ""
+  }
   </div>`;
-    
-  return newComment;
-}
 
+  return newComment;
+};
 
 btnSubmit.addEventListener("click", event => {
   event.preventDefault();
@@ -87,12 +143,21 @@ btnSubmit.addEventListener("click", event => {
     form.classList.add("was-validated");
   } else {
     const currentTimestamp = new Date().getTime();
-    const commentHTML = getCommentHTML(currentTimestamp, formUserName.value, formUserComment.value, isOffline());
+    const commentHTML = getCommentHTML(
+      currentTimestamp,
+      formUserName.value,
+      formUserComment.value,
+      isOffline()
+    );
     newCommentToDOM(commentHTML);
 
-    isOffline() 
-      ? saveToLocalStorage(currentTimestamp, formUserName.value, formUserComment.value)
-      : saveToDB(timestamp, formUserName.value, formUserComment.value);
+    isOffline()
+      ? saveToLocalStorage(
+          currentTimestamp,
+          formUserName.value,
+          formUserComment.value
+        )
+      : saveToDB(currentTimestamp, formUserName.value, formUserComment.value);
     form.classList.remove("was-validated");
     formUserComment.value = "";
     formUserName.value = "";
